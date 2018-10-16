cbd3b4a5dcd16b97871dac48c95db5a0b3d3e9ab
Chuck Dries <chuck@chuckdries.com>
2018-10-15 18:40:24 -0700

    Lecture 1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const twig = require('twig');
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'twig');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const messages = [
+    "howdy",
+    "hey there",
+    "new message",
+    "new new message"
+];
+app.get('/', (req, res) => {
+    // res.send('<h1>hello world!</h1>');
+    // res.render('index', { messages });
+    res.send(JSON.stringify(messages));
+});
+
+app.post('/message', (req, res) => {
+    messages.push(req.body.message);
+    res.redirect('/');
+})
+
+app.listen(3000);
+console.log('listening on port 3000');
