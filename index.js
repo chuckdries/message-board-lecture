0a6f0db90d3e32e6f64d2cdec0eb5a18a5e0156a
Chuck Dries <chuck@chuckdries.com>
2018-10-22 17:41:31 -0700

    add database querying

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const sqlite = require('sqlite');
 
 app.set('view engine', 'twig');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.disable('view cache');
+const dbPromise = sqlite.open('./data.db');
 
-const messages = [
-    "howdy",
-    "hey there"
-];
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const db = await dbPromise;
+    const messages = await db.all('SELECT * FROM messages;');
     res.render('index', { messages });
 });
 
-app.post('/message', (req, res) => {
-    messages.push(req.body.message);
+app.post('/message', async (req, res) => {
+    const db = await dbPromise;
+    await db.run('INSERT INTO messages (author, message) VALUES (?, ?)', req.body.author, req.body.message);
     res.redirect('/');
 })
 
