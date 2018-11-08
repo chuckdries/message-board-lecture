2bd8786355dda93f89b7c875ffcaf1eb6ff7fb6f
Chuck Dries <chuck@chuckdries.com>
2018-11-07 17:46:58 -0700

    clean up login and add login and register as separate pages

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ dbPromise.then(async (db) => {
     await db.run('CREATE TABLE IF NOT EXISTS sessions ( id INTEGER PRIMARY KEY, userid INTEGER, sessionToken STRING );');
 });
 
-// TODO: separate requireLogin function
+//asdf
 const authorize = async (req, res, next) => {
     // const { sessionToken } = req.cookies;
     const db = await dbPromise;
@@ -54,21 +54,53 @@ app.use(authorize);
 app.get('/', async (req, res) => {
     const db = await dbPromise;
     const messages = await db.all('SELECT * FROM messages;');
-    res.render('index', { messages, user: req.user });
+    const user = req.user;
+    res.render('index',{ messages, user });
+});
+
+app.get('/login', async (req, res) => {
+    if (req.user) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+app.get('/register', async (req, res) => {
+    if (req.user) {
+        res.redirect('/');
+        return;
+    }
+    res.render('register');
 });
 
 app.post('/message', requireAuth, async (req, res) => {
     const db = await dbPromise;
-    await db.run('INSERT INTO messages (author, message) VALUES (?, ?)', req.body.author, req.body.message);
+    await db.run('INSERT INTO messages (author, message) VALUES (?, ?)', req.user.email.split('@')[0], req.body.message);
     res.redirect('/');
 });
 
 app.post('/register', async (req, res) => {
     const db = await dbPromise;
+    const user = await db.get('SELECT * FROM users WHERE email=?', req.body.email);
+    if (user) {
+        res.status(400).render('register', { registerError: 'account already exists' });
+        return;
+    }
     const passwordHash = await bcrypt.hash(req.body.password, saltRounds);
-    await db.run('INSERT INTO users (email, passwordHash)  VALUES (?, ?);', req.body.email, passwordHash);
-    const users = await db.all('SELECT * FROM users');
-    console.log(users);
+    await db.run(
+        'INSERT INTO users (email, passwordHash)  VALUES (?, ?);',
+        req.body.email,
+        passwordHash
+    );
+    const newUser = db.get('SELECT id, email FROM users WHERE email=?', req.body.email);
+    const sessionToken = uuidv4();
+    await db.run(
+        'INSERT INTO sessions (userid, sessionToken) VALUES (?, ?);',
+        newUser.id,
+        sessionToken
+    );
+    res.cookie('sessionToken', sessionToken);
     res.redirect('/');
 });
 
@@ -76,7 +108,7 @@ app.post('/login', async (req, res) => {
     const db = await dbPromise;
     const user = await db.get('SELECT * FROM users WHERE email=?', req.body.email);
     if (!user) {
-        res.status(401).send('email or password is incorrect');
+        res.status(401).render('login', { loginError: 'email or password is incorrect' });
         return;
     }
     const passwordMatches = await bcrypt.compare(req.body.password, user.passwordHash);
@@ -86,7 +118,7 @@ app.post('/login', async (req, res) => {
         res.cookie('sessionToken', sessionToken);
         res.redirect('/');
     } else {
-        res.status(401).send('email or password is incorrect');
+        res.status(401).render('login', { loginError: 'email or password is incorrect' });
     }
 })
 
